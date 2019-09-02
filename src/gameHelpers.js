641fb73112bdd84066413b064cee7aed8bff5950
Eduardo Iglesias <eriglesias@ufl.edu>
2019-09-02 17:57:07 -0400

    check for collisions

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -6,3 +6,25 @@ export const createStage = () => {
     return new Array(STAGE_WIDTH).fill([0, 'clear'])
   })
 }
+
+export const checkCollision = (player, stage, {x: moveX, y: moveY}) => {
+  for(let y = 0; y < player.tetromino.length; y++) {
+    for(let x = 0; x < player.tetromino[y].length; x++) {
+
+      //1. Check that we are on a tetromino cell
+      if(player.tetromino[y][x] !== 0) {
+        if(
+        //2. Check that the move is within the game area's height(y)
+        !stage[y + player.pos.y +moveY] ||
+        //3. Check that move is inside game area's width(x)
+        !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+        //4. Check that the set we are moving to isn't clear
+        stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+      ) {
+        return true
+      }
+
+      }
+    }
+  }
+}
